3e21cf619ba5088f7145101aa23385523faf0565
桃子 <3181452262@qq.com>
2021-11-16 20:37:56 +0800

    null

---
+++ src/main/java/com/taozi/seckill/SeckillApplication.java
@@ -0,0 +1,13 @@
+package com.taozi.seckill;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.taozi.seckill.pojo")
+public class SeckillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}
---
+++ src/main/java/com/taozi/seckill/controller/DemoContoller.java
@@ -0,0 +1,20 @@
+package com.taozi.seckill.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoContoller {
+
+    /*
+     * 测试页面跳转
+     * */
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "taozi");
+        return "hello";
+    }
+}
